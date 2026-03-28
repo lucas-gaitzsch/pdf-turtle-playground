8668a60c7b1b30acea9ad03f47efd1373036234e
Lucas Gaitzsch <lucas.gaitzsch@visual-world.de>
2026-03-28 10:13:51 +0100

    use origin in prod build

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,3 +1,4 @@
-export const serverBaseUrl = ["localhost", "127.0.0.1"].includes(location.hostname)
-  ? "http://localhost:8000"
-  : location.origin
+export const serverBaseUrl =
+  import.meta.env.PROD || ["localhost", "127.0.0.1"].includes(location.hostname)
+    ? "http://localhost:8000"
+    : location.origin
